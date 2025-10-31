ab10d08813e5cdd413de4f29ee1c5901c9da49f4
mannnJAVA <mannn.html@gmail.com>
2025-10-31 18:18:01 +0530

    created a next.js project, Using the App Router, Building Server component

--- main/react-project/src/App.jsx
+++ main/react-project/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState, useReducer, useEffect } from "react";
 import "./App.css";
 import chef from "./images/chef.jpg";
 
@@ -22,32 +23,47 @@ const dishObjects = items.map((dish, i) => ({
   title: dish,
 }));
 
-function Main({ dishes }) {
+function Main({ dishes, openStatus, onStatus }) {
   return (
-    <main>
-      <img
-        src={chef}
-        height={200}
-        alt="A animated photo of smiling small boy chef"
-      />
-      <ul>
-        {dishes.map((dish, i) => (
-          <li key={dish.id} style={{ listStyleType: "none" }}>
-            {dish.title}
-          </li>
-        ))}
-      </ul>
-    </main>
+    <>
+      <div>
+        <button onClick={() => onStatus(true)}>I want to be open</button>
+        <h2>
+          Welcome to this beautiful restaurant ! {openStatus ? "Open" : "Close"}
+        </h2>
+      </div>
+      <main>
+        <img
+          src={chef}
+          height={200}
+          alt="A animated photo of smiling small boy chef"
+        />
+        <ul>
+          {dishes.map((dish, i) => (
+            <li key={dish.id} style={{ listStyleType: "none" }}>
+              {dish.title}
+            </li>
+          ))}
+        </ul>
+      </main>
+    </>
   );
 }
 
 function App() {
+  //const [status, setStatus] = useState(true);
+  const [status, toggle] = useReducer((status) => !status, true);
+
+  useEffect(() => {
+    console.log(`The restaurant is ${status ? "open" : "closed"}`);
+  }, [status]);
   return (
     <div>
+      <h1>The restaurant is currently {status ? "Open" : "Closed"}.</h1>
+      <button onClick={toggle}>{status ? "Close" : "Open"} Restaurant</button>
       <Header name="Mann" year={new Date().getFullYear()} />
-      <Main dishes={dishObjects} />
+      <Main dishes={dishObjects} openStatus={status} onStatus={toggle} />
     </div>
   );
 }
-
 export default App;
