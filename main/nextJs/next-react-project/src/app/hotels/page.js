e21dc342b8dc15658a69c746de1a8aa80163b0c7
mannnJAVA <mannn.html@gmail.com>
2025-10-31 19:34:10 +0530

    Passing data as props

--- main/nextJs/next-react-project/src/app/hotels/page.js
+++ main/nextJs/next-react-project/src/app/hotels/page.js
@@ -1,9 +1,33 @@
-export default function Page(){
-    return(
-        <main>
-            <div>
-                <h1>Hotel Details</h1>
-            </div>
-        </main>
-    )
+async function getData() {
+  const res = await fetch("https://snowtooth-hotel-api.fly.dev");
+  return res.json();
+}
+
+function HotelBlock({ name, capacity }) {
+  return (
+    <div>
+      <h2>{name}</h2>
+      <p>{capacity}</p>
+    </div>
+  );
+}
+export default async function Page() {
+  const data = await getData();
+  return (
+    <main>
+      <div>
+        <h1>Hotel Details</h1>
+        <h1>Hotel Details</h1>
+        <div>
+          {data.map((hotel) => (
+            <HotelBlock
+              key={hotel.id}
+              name={hotel.name}
+              capacity={hotel.capacity}
+            />
+          ))}
+        </div>
+      </div>
+    </main>
+  );
 }
